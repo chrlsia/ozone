0acbefe027ee9477f6747b3c1373a04bf680338b
Chris Siannas <xrsia@otenet.gr>
2019-11-09 13:14:31 +0200

    output all data

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
     require 'net/http'
     require 'json'
     @url ='http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=0&API_KEY=6C05FE7C-2BEA-4C0C-864E-9A739030C0D6'
-    @uri = URL(@url)
+    @uri = URI(@url)
     @response = Net::HTTP.get(@uri)
     @output=JSON.parse(@response)
 
