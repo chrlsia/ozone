32457f234e249fa2c67e469783b7093970677869
Chris Siannas <xrsia@otenet.gr>
2019-11-09 14:16:50 +0200

    Now is OK !!!

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     require 'net/http'
     require 'json'
-    @url ='http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=0&API_KEY=6C05FE7C-2BEA-4C0C-864E-9A739030C0D6'
+    @url ='http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=00000&distance=0&API_KEY=6C05FE7C-2BEA-4C0C-864E-9A739030C0D6'
     @uri = URI(@url)
     @response = Net::HTTP.get(@uri)
     @output=JSON.parse(@response)
@@ -10,24 +10,24 @@ class HomeController < ApplicationController
     #Check for empty return result
 
     if @output.empty? || !@output
-      @final_output = 'Error !!!!'
+      @final_output = 'Error'
     else
       @final_output = @output[0]['AQI']
     end
 
      if @final_output == 'Error'
       @api_color ="silver"
-     elsif @final_output <=50
+     elsif @final_output.to_i <=50
       @api_color ='green'
-     elsif @final_output>=51 && @final_output<=100
+     elsif @final_output.to_i>=51 && @final_output.to_i<=100
       @api_color="yellow"
-     elsif @final_output>=101 && @final_output<=150
+     elsif @final_output.to_i>=101 && @final_output.to_i<=150
       @api_color="orange"
-     elsif @final_output>=151 && @final_output<=200
+     elsif @final_output.to_i>=151 && @final_output.to_i<=200
       @api_color ="red"
-     elsif @final_output>=201 && @final_output<=300
+     elsif @final_output.to_i>=201 && @final_output.to_i<=300
       @api_color="purle"
-     elsif @final_output>=301 && @final_output<=500
+     elsif @final_output.to_i>=301 && @final_output.to_i<=500
       @api_color='maroon' 
      end
   end
