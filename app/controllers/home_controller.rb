3310bf027cf24407276930af75cb2f01f8e5f254
Chris Siannas <xrsia@otenet.gr>
2019-11-09 19:26:01 +0200

    zipcode 20002 and minor changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     require 'net/http'
     require 'json'
-    @url ='http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=0&API_KEY=6C05FE7C-2BEA-4C0C-864E-9A739030C0D6'
+    @url ='http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20002&distance=25&API_KEY=6C05FE7C-2BEA-4C0C-864E-9A739030C0D6'
     @uri = URI(@url)
     @response = Net::HTTP.get(@uri)
     @output=JSON.parse(@response)
